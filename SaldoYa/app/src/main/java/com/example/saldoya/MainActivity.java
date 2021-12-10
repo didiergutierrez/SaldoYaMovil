afd776ff2c090de80271fa65df97582acda7f958
CACJ18 <carrenocamiloaj18@gmail.com>
2021-12-09 23:57:39 -0500

    V1.0_Camilo
    Vista Registrar Empleado y Registrar Saldo

--- SaldoYa/app/src/main/java/com/example/saldoya/MainActivity.java
+++ SaldoYa/app/src/main/java/com/example/saldoya/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //String usuario;
-    Button b_login;
+    private Button b_login;
+    //private TextView lbl_Crear_Cuenta, lbl_Registrar_Saldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Buscamos el botón y se guarda en la variable
         b_login=findViewById(R.id.b_login);
+        //lbl_Crear_Cuenta= findViewById(R.id.lbl_Crear_Cuenta);
+        //lbl_Registrar_Saldo= findViewById(R.id.lbl_Registrar_Saldo);
 
         //Proceso o método al darle click al boton
         b_login.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //lbl_Crear_Cuenta.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent= new Intent(MainActivity.this, RegistrarEmpleado.class);
+        //        startActivity(intent);
+        //    }
+        //});
+
+        //lbl_Registrar_Saldo.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Intent intent= new Intent(MainActivity.this, RegistrarSaldo.class);
+        //        startActivity(intent);
+        //    }
+        //});
+
     }
 }
