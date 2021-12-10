d4c911446c9d0584d602e867fcb27f1057e8e4e9
jdcastilla10 <85692462+jdcastilla10@users.noreply.github.com>
2021-12-10 01:03:32 -0500

    mnbmnbm

---
+++ SaldoYa/app/src/main/java/com/example/saldoya/R_Tienda.java
@@ -0,0 +1,13 @@
+package com.example.saldoya;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class R_Tienda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rtienda);
+    }
+}
